6beebfffe85d787169477775acf84a538b59832c
CamieRealEstate <nguyenthanh168688@gmail.com>
2024-08-13 17:54:35 +0700

    Fix unit test

--- src/lib/components/home/home.component.spec.ts
+++ src/lib/components/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HomeComponent } from './home.component';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { provideTranslate } from '../../providers';
+
+import { HomeComponent } from './home.component';
 
 class MockActivatedRoute {}
 
@@ -10,8 +14,12 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [
+        HomeComponent,
+        TranslateModule.forRoot(),
+      ],
       providers: [
+        provideTranslate(),
         { provide: ActivatedRoute, useClass: MockActivatedRoute },
       ],
     }).compileComponents();
