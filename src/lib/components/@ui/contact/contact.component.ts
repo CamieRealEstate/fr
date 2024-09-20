cf7a3f851f364b8b8fb07eb8702a66afdedf7182
CamieRealEstate <nguyenthanh168688@gmail.com>
2024-09-20 13:09:26 +0700

    Implement send mail

--- src/lib/components/@ui/contact/contact.component.ts
+++ src/lib/components/@ui/contact/contact.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
+import { MatError } from '@angular/material/form-field';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import emailjs from '@emailjs/browser';
 import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 import { ListItem } from '../../../data';
@@ -9,23 +13,24 @@ import { ListItem } from '../../../data';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TranslateModule],
+  imports: [CommonModule, ReactiveFormsModule, TranslateModule, MatError, MatProgressBar],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent implements OnInit, OnDestroy {
-  private destroy$ = new Subject<void>();
-  
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private matSnackBar: MatSnackBar, private translate: TranslateService) { }
   contactForm!: FormGroup;
+  private destroy$ = new Subject<void>();
+  readonly isLoading = signal(false);
+  private date = new Date().toString();
 
   @Input() data: ListItem | undefined;
-  @Input() date: string | undefined;
 
   ngOnInit() {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
       title: ['[' + this.date + '] ' + this.data?.name + ' enquiry', Validators.required],
       message: ['I would like to learn more about ' + this.data?.name, []]
     });
@@ -42,14 +47,67 @@ export class ContactComponent implements OnInit, OnDestroy {
       this.contactForm.get('title')?.setValue(titleValue, { emitEvent: false });
     });
   }
-  
+
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
   onSubmit() {
-    console.log("submit");
-    console.log(this.contactForm.value);
+    this.showSuccessSnackbar();
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      this.sendEmail();
+    } else {
+      Object.values(this.contactForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.markAsTouched();
+        }
+      });
+    }
+  }
+
+  sendEmail() {
+    this.isLoading.set(true)
+    emailjs.init('biRBgw6KU04Oqs1LU');
+    emailjs.send('service_36vua4a', 'template_scplfby', {
+      title: this.contactForm.get('title')?.value || '',
+      message: this.contactForm.get('message')?.value || '',
+      name: this.contactForm.get('name')?.value || '',
+      phone: this.contactForm.get('phone')?.value || '',
+      email: this.contactForm.get('email')?.value || ''
+    })
+      .then(() => {
+        this.isLoading.set(false)
+        this.showSuccessSnackbar();
+      }, (error) => {
+        this.isLoading.set(false)
+        this.showErrorSnackbar();
+      });
+  }
+
+  private showSuccessSnackbar() {
+    this.translate.get('Email sent!').subscribe(
+      (translatedMessage: string) => {
+        this.matSnackBar.open(translatedMessage, 'Close', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    );
+  }
+
+  private showErrorSnackbar() {
+    this.translate.get('Error while sending email').subscribe(
+      (translatedMessage: string) => {
+        this.matSnackBar.open(translatedMessage, 'Close', {
+          duration: 2000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+        });
+      },
+    );
   }
 }
